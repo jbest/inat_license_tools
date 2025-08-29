6e74737e1791d2c250961744614730d9c11190db
Jason Best <jbest@brit.org>
2025-08-29 16:00:30 -0500

    getting photo metadata

--- inat_license_bulk.py
+++ inat_license_bulk.py
@@ -5,8 +5,8 @@ client = iNatClient()
 user_id='jbest'
 #user_id = 'luca_dt'
 
+"""
 observations = client.observations.search(user_id=user_id).all()
-
 #observations = get_observations(user_id=user_id)
 
 print(f'Total observations by {user_id}, {len(observations)}')
@@ -22,10 +22,15 @@ print('SAMPLE')
 #print(observations_filtered.first)
 #for obs in client.observations.search(user_id='my_username', taxon_name='Danaus plexippus'):
 #    print(obs) 
+"""
 
 #get single obs
-#102386279
-single_obs = client.observations(102386279)
+#102386279 by luca_dt https://www.inaturalist.org/observations/102386279
+obs_id = 102386279
+single_obs = client.observations(obs_id)
 print(single_obs)
 pprint(dir(single_obs))
-print(single_obs.license_code)
+print(f'License for observation {obs_id}: {single_obs.license_code}')
+#print(single_obs.photos)
+for photo in single_obs.photos:
+	print(f'Obs: {obs_id}, photo id: {photo.id}, license_code: {photo.license_code}')
